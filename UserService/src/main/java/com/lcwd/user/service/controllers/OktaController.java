f20a3c2e68ec3be4c2dfdab8129c7d7d464b402e
pilot-satyam <satyamsrivastaviit@gmail.com>
2023-12-21 18:43:20 +0530

    Controller Changes to Create User & Sync with DB

--- UserService/src/main/java/com/lcwd/user/service/controllers/OktaController.java
+++ UserService/src/main/java/com/lcwd/user/service/controllers/OktaController.java
@@ -1,5 +1,7 @@
 package com.lcwd.user.service.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -8,14 +10,19 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 // import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.repositories.UserRepository;
+
+import lombok.Data;
 
 @RestController
 @RequestMapping("/users")
@@ -28,44 +35,55 @@ public class OktaController {
     private final String apiToken = "00Zfzc-Vfc7FnNTlW4_dT_36-Re7CjD7Ec6RT4Feub";
 
     @PostMapping("/create")
-    public ResponseEntity<String> createUser() {
+    public ResponseEntity<String> createUser(@RequestBody RequestBodyStructure requestBody) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "SSWS " + apiToken);
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        String requestBody = "{ \"profile\": { \"firstName\": \"Isaac\", \"lastName\": \"Brock\", \"email\": \"isaac18@gmail.com\", \"login\": \"isaac18@gmail.com\" }, \"groupIds\": \"00gdgw8j7fc1gahSJ5d7\"}";
+        ObjectMapper objectMapper = new ObjectMapper();
 
-        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
-        RestTemplate restTemplate = new RestTemplate();
+        try {
+            String requestBodyString = objectMapper.writeValueAsString(requestBody);
+            HttpEntity<String> request = new HttpEntity<>(requestBodyString, headers);
+            RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, request, String.class);
+            ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, request, String.class);
 
-        if (response.getStatusCode() == HttpStatus.OK) {
-            try {
-                // Parse the response body to get user details
-                ObjectMapper objectMapper = new ObjectMapper();
+            if (response.getStatusCode() == HttpStatus.OK) {
                 JsonNode jsonNode = objectMapper.readTree(response.getBody());
-
                 String oktaId = jsonNode.get("id").asText();
-                String firstName = jsonNode.get("profile").get("firstName").asText();
-                String lastName = jsonNode.get("profile").get("lastName").asText();
-                String email = jsonNode.get("profile").get("email").asText();
+                String savedFirstName = jsonNode.get("profile").get("firstName").asText();
+                String savedLastName = jsonNode.get("profile").get("lastName").asText();
+                String savedEmail = jsonNode.get("profile").get("email").asText();
 
-                // Create a User instance and save it to the database
                 User user = new User();
                 user.setUserId(oktaId);
-                user.setName(firstName);
-                // user.setLastName(lastName);
-                user.setEmail(email);
+                user.setName(savedFirstName);
+                // user.setLastName(savedLastName);
+                user.setEmail(savedEmail);
 
                 userRepository.save(user);
 
                 return ResponseEntity.ok("User created and saved successfully!");
-            } catch (Exception e) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing response: " + e.getMessage());
+            } else {
+                return ResponseEntity.status(response.getStatusCode()).body("Failed to create user. Status code: " + response.getStatusCodeValue());
             }
-        } else {
-            return ResponseEntity.status(response.getStatusCode()).body("Failed to create user. Status code: " + response.getStatusCodeValue());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing request: " + e.getMessage());
         }
+    }
+
+    @Data
+    static class RequestBodyStructure {
+        private Profile profile;
+        private List<String> groupIds;
+    }
+
+    @Data
+    static class Profile {
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String login;
     }
 }
